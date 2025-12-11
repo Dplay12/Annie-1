069b553d7e021c1546007a3ffaa3503dd8efb2dc
Dplay12 <donovan.orozco.cdm.004@gmail.com>
2025-12-10 18:34:18 -0600

    casa.js

--- models/casa.js
+++ models/casa.js
@@ -1,4 +1,4 @@
-import * as THREE from '../JS/three.module.js';
+import * as THREE from '../js/three.module.js';
 
 export default function obj6({ x, y, z }) {
 
@@ -170,3 +170,4 @@ export default function obj6({ x, y, z }) {
 
     return casa;
 }
+
